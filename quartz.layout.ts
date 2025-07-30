d6fa1e2ebc07df6a2c9a05269a74353ccf4fe3f9
ajinasawor <puarenohajinasawor@gmail.com>
2025-07-30 17:23:10 +0700

    Quartz sync: Jul 30, 2025, 5:23 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,6 +17,15 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.ConditionalRender({
+      component: Component.Breadcrumbs(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+    Component.TagList(),
+  ],
+  left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
@@ -29,18 +38,8 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
-  //left: [
-
     //Component.Explorer(),
-  //],
+  ],
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
@@ -50,21 +49,20 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.PageTitle(),
-  Component.MobileOnly(Component.Spacer()),
-  Component.Flex({
-    components: [
-      {
-        Component: Component.Search(),
-        grow: true,
-      },
-      { Component: Component.Darkmode() },
-    ],
-  }),
-  Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  //left: [
-
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+      ],
+    }),
     //Component.Explorer(),
-  //],
+  ],
   right: [],
 }
